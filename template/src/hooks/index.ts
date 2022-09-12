d8d28b49c377bb5c885f40c39876b71a531ef449
Sultan Nadeem <sultanndilaram@gmail.com>
2022-09-12 17:12:30 +0500

    added useApi hook

--- template/src/hooks/index.ts
+++ template/src/hooks/index.ts
@@ -1,5 +1,5 @@
 export { default as useLocalStorage } from "./useLocalStorage";
-
 export { default as useRpc } from "./useRpc";
 export { default as useConfig } from "./useConfig";
 export { default as useProvider } from "./useProvider";
+export { default as useApi } from "./useApi";
---
+++ template/src/hooks/useApi.tsx
@@ -0,0 +1,84 @@
+import React from "react";
+import axios from "axios";
+import useConfig from "./useConfig";
+
+const HEADERS = {
+  "Content-Type": "application/json",
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization, X-Requested-With",
+  "Access-Control-Allow-Credentials": true,
+  Accept: "application/json",
+};
+
+interface Response<T = any> {
+  success: boolean;
+  code: number;
+  message: string;
+  data: T;
+}
+
+export default function useApi() {
+
+  const { offchainUrl: url } = useConfig();
+
+  const get = React.useCallback(async <T = any,>(endpoint: string, token: string | null): Promise<Response<T>> => {
+
+    if (endpoint[0] !== "/") throw new Error("Endpoint must start with a slash");
+
+    const response = await axios.get(url + endpoint, {
+      headers: token
+        ? { ...HEADERS, Authorization: `Bearer ${token}` }
+        : HEADERS,
+    });
+
+    return response.data;
+  }, [url]);
+
+  const post = React.useCallback(
+    async <T = any, R = any>(endpoint: string, data: T, token: string | null): Promise<Response<R>> => {
+
+      if (endpoint[0] !== "/") throw new Error("Endpoint must start with a slash");
+
+      const response = await axios.post(url + endpoint, data, {
+        headers: token
+          ? { ...HEADERS, Authorization: `Bearer ${token}` }
+          : HEADERS,
+      });
+
+      return response.data;
+    },
+    [url]
+  );
+
+  const put = React.useCallback(
+    async <T = any, R = any>(endpoint: string, data: T, token: string | null): Promise<Response<R>> => {
+
+      if (endpoint[0] !== "/") throw new Error("Endpoint must start with a slash");
+
+      const response = await axios.put(url + endpoint, data, {
+        headers: token
+          ? { ...HEADERS, Authorization: `Bearer ${token}` }
+          : HEADERS,
+      });
+
+      return response.data;
+    },
+    [url]
+  );
+
+  const del = React.useCallback(async <T = any,>(endpoint: string, token: string | null): Promise<Response<T>> => {
+
+    if (endpoint[0] !== "/") throw new Error("Endpoint must start with a slash");
+
+    const response = await axios.delete(url + endpoint, {
+      headers: token
+        ? { ...HEADERS, Authorization: `Bearer ${token}` }
+        : HEADERS,
+    });
+
+    return response.data;
+  }, [url]);
+
+  return { get, post, put, del };
+}
